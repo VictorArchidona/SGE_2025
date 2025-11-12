5c7930d877c84443d60e405994cd7d25d0b735a0
VictorArchidona <archidonav@gmail.com>
2025-11-12 12:55:26 +0100

    He termiando la tarea de Web Scrapping
    Finalizada 12/11/2025

--- Ejercicios/Web Scrapping/Web Scrapping.py
+++ Ejercicios/Web Scrapping/Web Scrapping.py
@@ -20,8 +20,16 @@ with open("Web Scrapping/titulo_xataka.txt", "w", encoding="utf-8") as fileTitul
     fileTitulo.write(titulo)
 
 # Extraer y guardar el primer elemento de lista <li>
-li = soup.find_all('li')
+li = soup.find_all("li", class_="masthead-nav-topics-item")
 
 with open("Web Scrapping/li_xataka.txt", "w", encoding="utf-8") as fileLi:
-    for item in li:
-        fileLi.write(item.get_text() + "\n")
+    for i in li:
+        fileLi.write(i.get_text() + "\n")
+
+#Extrae todo el texto de la noticia en limpio
+noticiaLimpia = soup.find_all("p")
+
+# Guardar la noticia en un archivo de texto
+with open("Web Scrapping/noticia_xatakaEntero.txt", "w", encoding="utf-8") as fileNoticia:
+    for p in noticiaLimpia:
+        fileNoticia.write(p.get_text() + "\n")
